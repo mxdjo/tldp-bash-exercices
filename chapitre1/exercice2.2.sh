244a4c36aaa596d64031a4c1cb1406da83425790
Mario <mariofadegnon@gmail.com>
2018-08-23 14:08:15 +0000

    1ère tentative d'exercice1

---
+++ chapitre1/exercice2.2.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+echo "La date d'aujourd'hui  est `date` "
+echo "Les utilisateurs connectés actuellement sont `users`"
+echo "Cela fait `uptime` que la machine est allumée"
---
+++ chapitre1/libelle-exercice2.2.md
@@ -0,0 +1,3 @@
+Write a script that upon invocation shows the time and date, lists all logged-in users, and gives the
+system uptime. The script then saves this information to a logfile.
+
